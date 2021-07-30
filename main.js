7e1defc7f1d0e24e4697ac3b6b4160f82b462751
tamalmaji <84242670+tamalmaji@users.noreply.github.com>
2021-07-30 19:17:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Opennab() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+function closenav(){
+    document.getElementById("mySidebar").style.display = "none"
+}
